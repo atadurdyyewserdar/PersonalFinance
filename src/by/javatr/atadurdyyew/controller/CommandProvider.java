f1ac90709fdaaa25bd9f92fdfe2b79851fc65f55
Atadurdyyew Serdar <atadurdyyewserdar@gmail.com>
2020-02-29 20:12:58 +0300

    Update Controller, added CommmandProvider class

---
+++ src/by/javatr/atadurdyyew/controller/CommandProvider.java
@@ -0,0 +1,31 @@
+package by.javatr.atadurdyyew.controller;
+
+import by.javatr.atadurdyyew.entity.CommandName;
+
+import java.util.HashMap;
+import java.util.Map;
+
+final public class CommandProvider {
+    private final Map<CommandName, Command> repository = new HashMap<>();
+
+    CommandProvider(){
+        repository.put(CommandName.LOG_IN, new LogIn());
+        repository.put(CommandName.LOG_OUT, new LogOut());
+        repository.put(CommandName.SIGN_UP, new SignUp());
+        repository.put(CommandName.CHARGE, new Charge());
+        repository.put(CommandName.ADD_FUND, new AddFund());
+    }
+
+    Command getCommand(String name){
+        CommandName commandName = null;
+        Command command = null;
+        try{
+            commandName = CommandName.valueOf(name.toUpperCase());
+            command = repository.get(commandName);
+        }
+        catch (IllegalArgumentException | NullPointerException ex){
+            ex.printStackTrace();
+        }
+        return command;
+    }
+}
